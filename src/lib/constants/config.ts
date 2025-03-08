f892f6bcb58982b672bfceb790a05857d87f9df4
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2025-03-08 09:00:14 +0500

    feat: update transit segment duration calculation and add stop wait time

--- src/lib/constants/config.ts
+++ src/lib/constants/config.ts
@@ -3,9 +3,11 @@ export const MAX_STATION_DISTANCE = 4; // kilometers
 /**
  * Maximum number of transfers to allow in routes
  */
-export const MAX_TRANSFERS = 5;
+export const MAX_TRANSFERS = 4;
 
 export const DISTANCE_MATRIX_API_URL =
   'https://maps.googleapis.com/maps/api/distancematrix/json';
 
 export const MAX_ROUTES_TO_RETURN = 3;
+
+export const STOP_WAIT_TIME_SECONDS = 30;
