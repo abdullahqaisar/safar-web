4867e277fd9a11ac3ffbcde3cd5fe25e0aec5769
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2025-03-28 07:18:45 +0500

    improve waiting time

--- src/core/utils/constants.ts
+++ src/core/utils/constants.ts
@@ -2,7 +2,7 @@
 export const EARTH_RADIUS_KM = 6371;
 export const AVG_WALKING_SPEED_KM_H = 5;
 export const AVG_WALKING_SPEED_M_MIN = (AVG_WALKING_SPEED_KM_H * 1000) / 60;
-export const DEFAULT_STOP_WAIT_TIME = 10; // seconds
+export const DEFAULT_STOP_WAIT_TIME = 20; // seconds
 export const WALKING_MAX_DISTANCE = 800; // meters
 export const INTERCHANGE_WALKING_TIME = 120; // seconds (time to change platforms)
 
